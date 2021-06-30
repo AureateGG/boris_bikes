951753022db9d6035611d7497f643783f9237019
thomas <thomas.lubon@icloud.com>
2021-06-30 15:50:51 +0100

    15:50

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,23 @@
 require "docking_station"
 describe DockingStation do 
     it { is_expected.to respond_to(:release_bike) }
+
+
+    it { is_expected.to respond_to(:dock).with(1).argument }
+
+    it { is_expected.to respond_to(:bike) }
+
+    it 'returns docked bikes' do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bike).to eq bike
+    end
+
+    describe '#release_bike' do
+        it 'releases a bike' do
+          bike = Bike.new
+          subject.dock(bike)
+          expect(subject.release_bike).to eq bike
+        end
+    end
 end
-#11:25
